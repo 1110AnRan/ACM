a6cb78376e4f866ca3b27fcc30086382badb82e1
安然 <2791158256@qq.com>
2023-05-04 18:34:58 +0800

    acm_code

---
+++ ACM集训/ACM第十四次集训(2023.4.23)/test.cpp
@@ -0,0 +1,173 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+// 2022年团体程序设计天梯赛-模拟赛
+
+//L1-1 自动编程
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//	int n;
+//	cin >> n;
+//	cout << "print(" << n << ")" << endl;
+//	return 0;
+//}
+
+//L1-2 太神奇了
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//	int a, b;
+//	cin >> a >> b;
+//	cout << a + b - 1 << endl;
+//	return 0;
+//}
+
+//L1-3 洛希极限
+//#include<iostream>
+//#include<iomanip>
+//using namespace std;
+//int main()
+//{
+//	double a, b;
+//	int c;
+//	cin >> a >> c >> b;
+//	double sum = 0;
+//	if (c == 0)
+//	{
+//		sum = a * 2.455;
+//	}
+//	else
+//	{
+//		sum = a * 1.26;
+//	}
+//	if (sum < b)
+//	{
+//		cout << fixed << setprecision(2) << sum << " ^_^" << endl;
+//	}
+//	else
+//	{
+//		cout << fixed << setprecision(2) << sum << " T_T" << endl;
+//	}
+//	return 0;
+//}
+
+//L1-4 吃鱼还是吃肉
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//	int N;
+//	cin >> N;
+//	int sex, height, weight;
+//	int i = 0;
+//	for (i = 0; i < N; i++)
+//	{
+//		cin >> sex >> height >> weight;
+//		if (sex == 0)
+//		{
+//			if (height == 129)
+//			{
+//				cout << "wan mei! ";
+//			}
+//			else if (height < 129)
+//			{
+//				cout << "duo chi yu! ";
+//			}
+//			else
+//			{
+//				cout << "ni li hai! ";
+//			}
+//			if (weight == 25)
+//			{
+//				cout << "wan mei!" << endl;
+//			}
+//			else if (weight < 25)
+//			{
+//				cout << "duo chi rou!" << endl;
+//			}
+//			else
+//			{
+//				cout << "shao chi rou!" << endl;
+//			}
+//		}
+//		else
+//		{
+//			if (height == 130)
+//			{
+//				cout << "wan mei! ";
+//			}
+//			else if (height < 130)
+//			{
+//				cout << "duo chi yu! ";
+//			}
+//			else
+//			{
+//				cout << "ni li hai! ";
+//			}
+//			if (weight == 27)
+//			{
+//				cout << "wan mei!" << endl;
+//			}
+//			else if (weight < 27)
+//			{
+//				cout << "duo chi rou!" << endl;
+//			}
+//			else
+//			{
+//				cout << "shao chi rou!" << endl;
+//			}
+//		}
+//	}
+//	return 0;
+//}
+
+//L1-5 不变初心数
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+	int n;
+	cin >> n;
+	int i;
+	int a;
+	for (i = 0; i < n; i++)
+	{
+
+	}
+	return 0;
+}
+
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//
+//	return 0;
+//}
+//
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//
+//	return 0;
+//}
+//
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//
+//	return 0;
+//}
+//
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//
+//	return 0;
+//}
